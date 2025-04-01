bacbc698d2a6f189a2183844ea53e88d634ded27
GerwindeHeus <90325732+GerwindeHeus@users.noreply.github.com>
2025-04-01 12:12:13 +0200

    kleur aangepast en insta in de hamburger menu gezet

--- script.js
+++ script.js
@@ -3,25 +3,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu');
     const menuLogo = document.querySelector('.menu-logo');  
     const menuIcon = document.querySelector('.menu-small-icon');
-
-    hamburger.addEventListener('click', () => {
-        menu.classList.toggle('active'); 
-        
-        if (hamburger.classList.contains('fa-bars')) {
-            hamburger.classList.remove('fa-bars');
-            hamburger.classList.add('fa-times');
-        } else {
+  
+    const checkScreenSize = () => {
+        if (window.innerWidth >= 992) {
+            menu.classList.remove('active');
             hamburger.classList.remove('fa-times');
             hamburger.classList.add('fa-bars');
-        }
-
-        // Toon of verberg het logo
-        if (menu.classList.contains('active')) {
-            menuLogo.style.display = 'block'; 
-            menuIcon.style.display = 'block';
-        } else {
-            menuLogo.style.display = 'none'; 
+            menuLogo.style.display = 'none';
             menuIcon.style.display = 'none';
+        }
+    };
+
+    checkScreenSize();
+
+    window.addEventListener('resize', checkScreenSize);
+
+    hamburger.addEventListener('click', () => {
+        if (window.innerWidth < 992) {
+            menu.classList.toggle('active'); 
+            
+            if (hamburger.classList.contains('fa-bars')) {
+                hamburger.classList.remove('fa-bars');
+                hamburger.classList.add('fa-times');
+            } else {
+                hamburger.classList.remove('fa-times');
+                hamburger.classList.add('fa-bars');
+            }
+
+            if (menu.classList.contains('active')) {
+                menuLogo.style.display = 'block'; 
+                menuIcon.style.display = 'block';
+            } else {
+                menuLogo.style.display = 'none'; 
+                menuIcon.style.display = 'none';
+            }
         }
     });
 });
